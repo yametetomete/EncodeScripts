d0014607bdbb0f04bb9acf284629bdaf898b4ee5
louis <heicrd@cock.li>
2020-04-27 14:40:13 -0400

    xv: bd: vol 1

---
+++ Symphogear/XV [BD]/02/ep2pv_ac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import acsuite
+import os
+import vapoursynth as vs
+
+core = vs.core
+
+ac = acsuite.AC()
+path = r"bdmv/KIXA_90888/BDMV/STREAM/00006.m2ts"
+src = core.lsmas.LWLibavSource(path)
+ac.eztrim(src, [(0, -24)], os.path.splitext(path)[0] + ".wav", "ep2pv.wav")
