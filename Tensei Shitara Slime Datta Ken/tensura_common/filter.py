94cf2ae726e17097be1978b8838a768c863aecf4
louis <louis@poweris.moe>
2021-07-01 10:09:50 -0400

    tensura: s2: tv: recap 2

--- Tensei Shitara Slime Datta Ken/tensura_common/filter.py
+++ Tensei Shitara Slime Datta Ken/tensura_common/filter.py
@@ -14,7 +14,8 @@ from lvsfunc.types import Range
 from awsmfunc import bbmod
 from debandshit import f3kbilateral
 from kagefunc import retinex_edgemask
-from vardefunc import dumb3kdb, fsrcnnx_upscale
+from vardefunc.deband import dumb3kdb
+from vardefunc.scale import fsrcnnx_upscale
 from vsutil import depth
 from vsutil import Range as CRange
 
@@ -67,6 +68,12 @@ def deband(clip: vs.VideoNode, strong: Optional[List[Range]] = None,
     return core.std.MaskedMerge(deb, clip, dmask)
 
 
+def deband_tv(clip: vs.VideoNode) -> vs.VideoNode:
+    dmask = detail_mask(clip)
+    deb = dumb3kdb(clip, radius=16, threshold=40)
+    return core.std.MaskedMerge(deb, clip, dmask)
+
+
 def antialias(clip: vs.VideoNode, strong: Optional[List[Range]] = None,
               sangnom: Optional[List[Tuple[Range, List[BoundingBox]]]] = None) -> vs.VideoNode:
     clamp = sraa_clamp(clip, mask=mask_strong)
@@ -95,6 +102,18 @@ def regrain(clip: vs.VideoNode) -> vs.VideoNode:
     sgrain_h = core.std.MaskedMerge(clip, sgrain_h, mask_dark.std.Invert())
     sgrain = sgrain_h.std.MergeDiff(clip.std.MakeDiff(sgrain_l))
     dgrain = core.std.MaskedMerge(clip, clip.grain.Add(var=0.35, uvar=0.1, constant=False, seed=393), mask_dark)
+    grain = dgrain.std.MergeDiff(clip.std.MakeDiff(sgrain))
+    return grain
+
+
+def regrain_tv(clip: vs.VideoNode) -> vs.VideoNode:
+    mask_bright = clip.std.PlaneStats().adg.Mask(10)
+    mask_dark = clip.std.PlaneStats().adg.Mask(25)
+    sgrain_l = core.std.MaskedMerge(clip, clip.grain.Add(var=0.1, constant=True, seed=393), mask_bright.std.Invert())
+    sgrain_h = core.std.MaskedMerge(clip, clip.grain.Add(var=0.15, uvar=0.1, constant=True, seed=393), mask_bright)
+    sgrain_h = core.std.MaskedMerge(clip, sgrain_h, mask_dark.std.Invert())
+    sgrain = sgrain_h.std.MergeDiff(clip.std.MakeDiff(sgrain_l))
+    dgrain = core.std.MaskedMerge(clip, clip.grain.Add(var=0.25, uvar=0.1, constant=False, seed=393), mask_dark)
     grain = dgrain.std.MergeDiff(clip.std.MakeDiff(sgrain))
     return grain
 
